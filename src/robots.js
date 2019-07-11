273f99530f733c36a5193db637da1bdd18111359
Mary <mariamihaila1409@gmail.com>
2019-07-11 21:21:36 +0300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Stacee Boothe",
+        username: "sboothe0",
+        email: "sboothe0@java.com"
+    },
+    {
+        id: 2,
+        name: "Jodie Mazzey",
+        username: "jmazzey1",
+        email: "jmazzey1@weather.com"
+    },
+    {
+        id: 3,
+        name: "Sol Nurcombe",
+        username: "snurcombe2",
+        email: "snurcombe2@amazon.com"
+    },
+    {
+        id: 4,
+        name: "Maryanna Tarte",
+        username: "mtarte3",
+        email: "mtarte3@hatena.ne.jp"
+    },
+    {
+        id: 5,
+        name: "Augustin Hartmann",
+        username: "ahartmann4",
+        email: "ahartmann4@sciencedirect.com"
+    },
+    {
+        id: 6,
+        name: "Lexy Notman",
+        username: "lnotman5",
+        email: "lnotman5@dagondesign.com"
+    },
+    {
+        id: 7,
+        name: "Nina Berrecloth",
+        username: "nberrecloth6",
+        email: "nberrecloth6@instagram.com"
+    },
+    {
+        id: 8,
+        name: "Shannan Monnoyer",
+        username: "smonnoyer7",
+        email: "smonnoyer7@pinterest.com"
+    },
+    {
+        id: 9,
+        name: "Eveleen Rhelton",
+        username: "erhelton8",
+        email: "erhelton8@ning.com"
+    },
+    {
+        id: 10,
+        name: "Brenna Ding",
+        username: "bding9",
+        email: "bding9@noaa.gov"
+    },
+    {
+        id: 11,
+        name: "Justinian Ebbin",
+        username: "jebbina",
+        email: "jebbina@biglobe.ne.jp"
+    },
+    {
+        id: 12,
+        name: "Fraser Gibard",
+        username: "fgibardb",
+        email: "fgibardb@printfriendly.com"
+    },
+    {
+        id: 13,
+        name: "Rafa Wermerling",
+        username: "rwermerlingc",
+        email: "rwermerlingc@sitemeter.com"
+    },
+    {
+        id: 14,
+        name: "Syd Theobalds",
+        username: "stheobaldsd",
+        email: "stheobaldsd@g.co"
+    },
+    {
+        id: 15,
+        name: "Brockie Stranio",
+        username: "bstranioe",
+        email: "bstranioe@bing.com"
+    }
+]
